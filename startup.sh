0ca02642b95babd74204c50f24205f6de79d514c
chtandy <39547245+chtandy@users.noreply.github.com>
2021-06-08 17:17:30 +0800

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -16,5 +16,6 @@ if [ ! -z $PRIVATEKEYNAME ] && [ ! -z $PUBLICKEYNAME ]; then
     sed -i -e "s|^publicKey.*|publicKey=/data/$PUBLICKEYNAME|" /Bastillion-jetty/jetty/bastillion/WEB-INF/classes/BastillionConfig.properties
 fi   
 
+
 cd jetty;
 java ${JAVA_OPTS} -jar start.jar

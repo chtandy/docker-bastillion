02e055bc705b381121cc984629e9e82be55e4608
chtandy <cht.andy@gmail.com>
2022-07-15 09:27:39 +0000

    update

--- startup.sh
+++ startup.sh
@@ -4,18 +4,20 @@ SourceFile=/Bastillion-jetty/jetty/bastillion/WEB-INF/classes/bastillion.jceks
 DistinationFile=/Bastillion-jetty/jetty/bastillion/WEB-INF/classes/keydb/bastillion.jceks
 BatillionConfig=/Bastillion-jetty/jetty/bastillion/WEB-INF/classes/BastillionConfig.properties
 
-if [ ! -f $DistinationFile ];  then
-    mv $SourceFile $DistinationFile
-    ln -sf $DistinationFile $SourceFile
-else
-    rm -f $SourceFile
-    ln -sf $DistinationFile $SourceFile
+function savejceks {
+    cp $SourceFile $DistinationFile
+}
+
+trap savejceks EXIT
+
+if [ -f $DistinationFile ];  then
+    cp $DistinationFile $SourceFile
 fi
+
 
 sed -i -e 's|^dbPassword.*|dbPassword=Bastillion-jetty|' ${BatillionConfig}
 sed -i -e 's|^keyManagementEnabled.*|keyManagementEnabled=false|' ${BatillionConfig}
 sed -i -e 's|^forceUserKeyGeneration.*|forceUserKeyGeneration=false|' ${BatillionConfig}
-
 
 
 if [ ! -z $PRIVATEKEYNAME ] && [ ! -z $PUBLICKEYNAME ]; then
@@ -26,7 +28,6 @@ fi
 if [ ${EnableInternalAudio} == 'true' ]; then
     sed -i -e "s|enableInternalAudit.*|enableInternalAudit=true|" ${BatillionConfig}
 fi
-
 
 cd /Bastillion-jetty/jetty;
 java ${JAVA_OPTS} -jar start.jar
